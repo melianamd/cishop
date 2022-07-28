496e6e0836ec0b410dca07ecfb776293ffa6462f
melianamd <devimonica40@gmail.com>
2022-07-28 18:44:18 +0700

    initialize file

---
+++ application/views/layouts/_navbar.php
@@ -0,0 +1,58 @@
+<nav class="navbar navbar-expand-md navbar-light fixed-top bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">Thrift Bymelys</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+      <ul class="navbar-nav me-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>
+        </li>
+        
+    <div class="menu" aria-labelledby="dropdown-1">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item active"><a href="#" class="nav-link dropdown-toggle" id="dropdown-1" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Manage</a>
+            <ul class="submenu">
+             <li class="nav-item active"><a class="dropdown-item" href="/admin-category.html">Kategori</a></li>
+             <li class="nav-item active"><a class="dropdown-item" href="/admin-product.html">Produk</a></li>
+             <li class="nav-item active"><a class="dropdown-item" href="/admin-order.html">Order</a></li>
+             <li class="nav-item active"><a class="dropdown-item" href="/admin-users.html">Pengguna</a></li>
+            </ul>
+         </li>
+        </ul>
+    </div>
+    </ul>
+      
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a href="/cart.html" class="nav-link"><i class="fas fa-shopping-cart"></i> Cart (0)</a>
+      </li>
+      <?php if (!$this->session->userdata('is_login')) : ?>
+      <li class="nav-item">
+        <a href="<?= base_url('/login') ?>" class="nav-link">Login</a>
+      </li>
+
+      <li class="nav-item">
+        <a href="<?= base_url('/register') ?>" class="nav-link">Register</a>
+      </li>
+      <?php else : ?>
+      <div class="menu" aria-labelledby="dropdown-2">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item active"><a href="#" class="nav-link dropdown-toggle" id="dropdown-2" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><?= $this->session->userdata('name') ?></a>
+            <ul class="submenu">
+             <li class="nav-item active"><a class="dropdown-item" href="/profil.html">Profil</a></li>
+             <li class="nav-item active"><a class="dropdown-item" href="/orders.html">Order</a></li>
+             <li class="nav-item active"><a class="dropdown-item" href="<?= base_url('/logout') ?>">Logout</a></li>
+            </ul>
+         </li>
+         <?php endif ?>
+        </ul>
+    </div>
+
+
+    </ul>
+
+    </div>
+  </div>
+</nav>
---
+++ application/views/layouts/app.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.88.1">
+    <title><?= isset($title) ? $title : 'CIShop' ?> CodeIgniter E-commerce</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.1/examples/navbar-fixed/">
+    
+
+    <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="/assets/libs/bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="/assets/css/app.css">
+
+    <!-- Font Awesome CSS -->
+<link rel="stylesheet" href="/assets/libs/fontawesome/css/all.min.css">
+
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="/docs/5.1/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="/docs/5.1/assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="/docs/5.1/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+<link rel="icon" href="/docs/5.1/assets/img/favicons/favicon.ico">
+<meta name="theme-color" content="#7952b3">
+
+    
+    <!-- Custom styles for this template -->
+    <link href="/assets/css/app.css" rel="stylesheet">
+  </head>
+  <body>
+    
+    <!-- Navbar -->
+    <?php $this->load->view('layouts/_navbar'); ?>
+    <!-- End Navbar -->
+
+    <!-- Content -->
+    <?php $this->load->view($page); ?>
+    <!-- End Content -->
+
+
+<script src="/assets/libs/jquery/jquery-3.6.0.min.js"></script>
+<script src="/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="/assets/js/app.js"></script>
+
+</body>
+</html>
