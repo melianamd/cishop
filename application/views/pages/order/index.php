1ef05e65abc807c5b98c364b7f79a9fd1d1dbc84
melianamd <devimonica40@gmail.com>
2022-11-18 13:35:23 +0700

    memperbaiki view manage order

--- application/views/pages/order/index.php
+++ application/views/pages/order/index.php
@@ -1,25 +1,25 @@
 <main class="container">
     <?php $this->load->view('layouts/_alert'); ?>
     <div class="row">
-        <div class="col-md-10 mx-auto">
+        <div class="col-md-12 mb-5 mt-5">
             <div class="card">
                 <div class="card-header">
-                  <span>Order</span>
-                  <div class="float-end">
-						<?= form_open(base_url('order/search'), ['method' => 'POST']) ?>
-							<div class="input-group">
-								<input type="text" name="keyword" class="form-control form-control-sm text-center" placeholder="Cari" value="<?= $this->session->userdata('keyword') ?>">
-								<div class="input-group-append">
-									<button class="btn btn-info btn-sm" type="submit">
-										<i class="fas fa-search"></i>
-									</button>
-									<a href="<?= base_url('order/reset') ?>" class="btn btn-info btn-sm">
-										<i class="fas fa-eraser"></i>
-									</a>
-								</div>
-							</div>
-						<?= form_close() ?>
-					</div>
+                    <span>Manage Order</span>
+                    <div class="float-end">
+                        <?= form_open(base_url('order/search'), ['method' => 'POST']) ?>
+                        <div class="input-group">
+                            <input type="text" name="keyword" style=" width: 150px; height: 37px;" class="form-control form-control-sm text-center" placeholder="Cari" value="<?= $this->session->userdata('keyword') ?>">
+                            <div class="input-group-append">
+                                <button class="btn bg-gradient-dark" style="height:37px;" type="submit">
+                                    <i class="fas fa-search"></i>
+                                </button>
+                                <a href="<?= base_url('order/reset') ?>" class="btn bg-gradient-dark" style="height:37px ;">
+                                    <i class="fas fa-eraser"></i>
+                                </a>
+                            </div>
+                        </div>
+                        <?= form_close() ?>
+                    </div>
                 </div>
                 <div class="card-body">
                     <table class="table">
@@ -32,27 +32,27 @@
                             </tr>
                         </thead>
                         <tbody>
-                        <?php foreach($content as $row) : ?>
-                            <tr>
-                                <td>
-                                    <a href="<?= base_url("/order/detail/$row->id") ?>"><strong><?= $row->invoice ?></strong></a>
-                                </td>
-                                <td><?= str_replace('-', '/', date("d-m-Y", strtotime($row->date))) ?></td>
-                                <td>Rp<?= number_format ($row->total, 0, ',' , '.') ?> ,-</td>
-                                <td>
-                                    <?php $this->load->view('layouts/_status', ['status' => $row->status]); ?>
-                                </td>
-                            </tr>
-                        <?php endforeach ?>
+                            <?php foreach ($content as $row) : ?>
+                                <tr>
+                                    <td>
+                                        <a href="<?= base_url("/order/detail/$row->id") ?>"><strong><?= $row->invoice ?></strong></a>
+                                    </td>
+                                    <td><?= str_replace('-', '/', date("d-m-Y", strtotime($row->date))) ?></td>
+                                    <td>Rp<?= number_format($row->total, 0, ',', '.') ?> ,-</td>
+                                    <td>
+                                        <?php $this->load->view('layouts/_status', ['status' => $row->status]); ?>
+                                    </td>
+                                </tr>
+                            <?php endforeach ?>
                         </tbody>
                     </table>
-                 
-                 <nav aria-label="Page navigation example">
-                  <?= $pagination ?>
-                </nav>
+
+                    <nav aria-label="Page navigation example">
+                        <?= $pagination ?>
+                    </nav>
                 </div>
-              </div>
+            </div>
         </div>
     </div>
-  
+
 </main>
